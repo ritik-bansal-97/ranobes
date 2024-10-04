47a63dbfbc0cee041468fe51971cace98725f6d7
Ritik Bansal <ritikbansal@Ritiks-MacBook-Air.local>
2024-10-04 22:21:26 +0530

    gh page addition

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LoginPage from './components/LoginPage/LoginPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <Router>
+    <Router basename="/ranobes">
       <div className="App">
         <Header />
         <Routes>
